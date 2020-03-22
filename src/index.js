835d58f8cd496cf1530154c02cc7378a4de37873
Andrei Harbuz <nerev94@gmail.com>
2020-03-23 00:35:26 +0300

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    let result = [];
+    if (!Array.isArray(matrix)) return [];
+    matrix.forEach( (array, index) => {
+        if (index % 2 == 0) {
+            array.sort( (a, b) => a - b);
+        } else {
+            array.sort( (a, b) => b - a);
+        }
+        array.forEach( item => {
+            result.push(item);
+        });
+    })
+    return result;
 }
